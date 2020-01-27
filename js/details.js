615f372463f1cc0d0fbb703d1e3ff5ba22fdd7b5
Bayron <bayrondaymir@yahoo.es>
2020-01-27 16:32:03 -0500

    Complete modals

--- js/details.js
+++ js/details.js
@@ -8,29 +8,45 @@ $(document).ready(function () {
         response = "";
         switch (e.target.innerText) {
             case "Html5":
-              response = "Hice un curso muy corto (alrededor de unas 10 horas) por Internet sobre HTML y con eso aprendi a manejarlo ";
+              response = "Hice un curso muy corto (alrededor de unas 10 horas) por Internet sobre HTML y con eso aprendi a manejarlo de manera basica.";
               break;
             case "CSS3":
-              response = "";
+              response = "Antes de este ejercicio conocia los aspectos basicos sobre CSS sin embargo no lo habia utilizado antes.";
               break;
             case "Javascript":
-               response = "";
+               response = "Hice un curso muy corto por Internet sobre HTML y con eso aprendi a manejarlo de manera basica, cabe aclarar que el curso tenia pequeños proyectos que me ayudaron a entenderlo muy bien.";
               break;
-            case "Vue":
+            case "Vue": "Conoci este Framework en el curso de Proyecto Integrador I, sin embargo no lo domino totalmente."
               response = "";
               break;
-            case 4: "Communication"
-              response = "";
+            case "Communication": 
+              response = "Me considero una persona que puede expresar sus ideas de manera clara y concisa.";
               break;
-            case 5: "Teamwork"
-              response = "";
+            case "Teamwork": 
+              response = "El trabajo en equipo no es mi fuerte pero considero que me desenvuelvo bien siempre y cuando se definan de manera efectiva las tareas de los integrantes del equipo.";
               break;
-            case 6: "Creativity"
-              response = "";
+            case "Creativity": 
+              response = "Me considero una persona creativa dado que siempre estoy pensando en maneras diferentes y mejores de hacer las cosas, incluso en las actividades mas sencillas de la vida diaria";
+              break;
+            case "Dedication": 
+              response = "Me considero una persona dedica dado que pongo el mayor esfuerzo en las cosas que realizo todos los dias con el fin de que queden lo mejor posible";
+              break;
+            case "Spring": 
+              response = "Este Framework basado en Java lo utilice durante un año en trabajos universitarios especificamente en las materias de Commputacion en Internet y Proyecto Integrador 2.";
+              break;
+            case "Git": 
+              response = "Aprendi ha usar esta herramienta en un curso de Git en Platzi.com";
+              break;
+            case "React": 
+              response = "Me interesa conocer más sobre esta libreria dado que es una de las más demandadas en el desarrollo web actualmente. Tengo conocimientos muy basicos sobre como funciona.";
+              break;
+            case "Android Studio": 
+              response = "Este IDE lo empece a usar hace 4 meses en el curso universitario de Aplicaciones Moviles donde lo aprendimos a manejar de manera avanzada desde la creación de un proyecto hasta la publicacion de una aplicación movil en la Play Store de Google.";
           }
 
           if(response != ""){
             modal.style.display = "block"
+            $('.modal-content h2').html("Sobre " + e.target.innerText);
             $('.modal-content p').html(response);
             
           }
